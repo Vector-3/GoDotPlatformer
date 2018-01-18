adfe0a4f6315648eb49f50674dab3a6b19e8724e
Liam Harrison <liam.harrison12345@gmail.com>
2018-01-18 15:01:44 +1100

    Add some more stuff

--- Scripts/Controllers/PlayerController.cs
+++ Scripts/Controllers/PlayerController.cs
@@ -3,34 +3,55 @@ using System;
 
 public class PlayerController : KinematicBody2D
 {
-	private const float force = 250;
+	private const float walkSpeed = 250;
 	private Vector2 gravity = new Vector2(0, 250);
-
+	private bool debugMode = true;
+	private RichTextLabel debugLabel; 
+	private Vector2 velocity = new Vector2(0,0);
+	
     public override void _Ready()
     {
         // Called every time the node is added to the scene.
         // Initialization here
-        
+        debugLabel = (RichTextLabel)GetNode("/root/Scene/CanvasLayer/DebugLabel");
     }
 
     public override void _Process(float delta)
     {
         // Called every frame. Delta is time since last frame.
         // Update game logic here.
-        
-		Vector2 moveDir = new Vector2(0,0);
+		
+        if (debugMode && debugLabel != null) 
+		{
+			debugLabel.Text = "X: " + GetPosition().x.ToString("0") + ", Y: " + GetPosition().y.ToString("0");
+			debugLabel.Text += "\nCell X: " + (GetPosition().x / 32).ToString("0") + ", Cell Y: " + (GetPosition().y / 32).ToString("0");
+			debugLabel.Text += "\n\nOnFloor: " + IsOnFloor();
+			debugLabel.Text += "\nOnWall: " + IsOnWall();
+		}
+    }
+
+	public override void _PhysicsProcess(float delta) 
+	{
+		
+		velocity += gravity * delta;
 		
 		if (Input.IsActionPressed("character_left"))
 		{
-			moveDir += new Vector2(-1, 0);
+			velocity.x = -walkSpeed;
 		}
-		if (Input.IsActionPressed("character_right"))
+		else if (Input.IsActionPressed("character_right"))
 		{
-			moveDir += new Vector2(1, 0);
+			velocity.x = walkSpeed;
+		}
+		else
+		{
+			velocity.x = 0;
 		}
 		
-		MoveAndCollide(gravity * delta);
-		MoveAndCollide(moveDir * force * delta);
-    }
+		var motion = velocity * delta;
+		
+		MoveAndSlide(velocity);
+		
+	}
 	
 }
